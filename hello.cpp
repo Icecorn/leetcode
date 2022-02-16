4fc4aeb10c9530af1a935766ae9af8fafcd095d8
JunPC <jun@MacBook-Pro-2.local>
2022-02-16 16:54:35 +0800

    首次提交

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{   int a;
+    a = 19;
+    std:: vector<int> stack;
+    stack.__emplace_back(a);
+    std::cout << a <<"aaaaaa"<< std::endl;
+    std::cout << "塑料袋咖啡节奏" << std::endl;
+}
